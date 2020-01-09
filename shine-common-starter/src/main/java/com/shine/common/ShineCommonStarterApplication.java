1fa74060082be949a73a64f74a8594c5350da775
weiqiang <1432114216@qq.com>
2020-01-09 21:30:49 +0800

    初始化common
    Signed-off-by: weiqiang <1432114216@qq.com>

---
+++ shine-common-starter/src/main/java/com/shine/common/ShineCommonStarterApplication.java
@@ -0,0 +1,13 @@
+package com.shine.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShineCommonStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShineCommonStarterApplication.class, args);
+    }
+
+}
---
+++ shine-common-starter/src/test/java/com/shine/common/ShineCommonStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shine.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShineCommonStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
